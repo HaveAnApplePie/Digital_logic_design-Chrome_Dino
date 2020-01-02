9adcc73fda198d389ab4bd842e464a5fd2bf2162
HaveAnApplePie <50864986+HaveAnApplePie@users.noreply.github.com>
2020-01-02 20:21:05 +0800

    Cactus_s.v.1.2
    在Cactus_s.v中用了IP核来获取像素的值，同时加了障碍物输出模块

--- Cactus_s.v
+++ Cactus_s.v
@@ -19,6 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Cactus_s(					 //小仙人掌 small cactus
+	 input wire clk_ip,			 //给IP核用的时钟信号，频率更高，clk_div[1]
 	 input wire clk,				 //物体向前移动一列的时钟信号
 	 input wire start,			 //开始信号,为一个短脉冲  接到短脉冲之后开始输出图像
 	 input wire [9:0] col_addr, // pixel ram row address, 480 (512) lines
@@ -27,25 +28,26 @@ module Cactus_s(					 //小仙人掌 small cactus
 	 output reg finish			 //完成信号
     );
 	 parameter	
-		HEIGHT  	   = 80,			 //图像高度
-		LENGTH      = 60,			 //图像长度
+		HEIGHT  	   = 10,			 //图像高度
+		LENGTH      = 8,			 //图像长度
 		COLNUM      = 640,	    //总列数
 		ROW_HIGHEST = 130;	    //该模块所在的最高的行数，以0开始
 
 	//这里可以用IP核代替
-	 reg [11:0]data[0:HEIGHT*LENGTH - 1];  //总共HEIGHT*LENGTH个data，每个为12位数据
-	 reg [16:0]addr;								//对应于       ||||||||||■ ■||||||||||      的位置
+	 reg [11:0]data;  							//每个为12位数据
+	 reg [15:0]addr;								//对应于       ||||||||||■ ■||||||||||      的位置
 														//            ||		 ||■■■||		  ||
 														//				  |||||||||| ■ ||||||||||
 	 initial begin
-		$readmemh("Cactus_s.hex", data);
 		addr  <= 0;
 		count <= LENGTH+COLNUM;					//保证一开始finish一直为1，从而输出一直为白色
 	 end
 	//////
 	
+	Cactus_s_ip CACTUS(clk_ip,addr,data);
 	
-	reg [11：0]count;				 //起始列的位置
+	
+	reg [11: 0]count;				 //起始列的位置
 	always @(posedge clk or posedge start) begin
 		if(start)					 //start由0变为1时,开始输出图像
 			count <= 0;
@@ -55,15 +57,19 @@ module Cactus_s(					 //小仙人掌 small cactus
 	
 //	assign addr = row_addr*COLNUM+col_addr+count; 	//按行放图像像素点的值时
 	
-	assign addr	 =	(row_addr-(ROW_HIGHEST-HEIGHT+1))*LENGTH+			//对应文件里的行
-			         count+col_addr-COLNUM;						         //对应文件里的列
 	
 
-   if(count>=LENGTH+COLNUM)	 //如果已经移出，则输出finish信号
-		finish = 1;
-	else
-		finish = 0;
-
+	always@(*)begin
+		addr	 <=	(row_addr-(ROW_HIGHEST-HEIGHT+1))*LENGTH+			//对应文件里的行
+			          count+col_addr-COLNUM;						         //对应文件里的列
+		
+		if(count>=LENGTH+COLNUM)	 //如果已经移出，则输出finish信号
+			finish <= 1;
+		else
+			finish <= 0;
+	end
+	
+	
 	always @(*) begin
 		if(!finish)begin	//未完成输出
 			if ((count+col_addr) >= COLNUM && 				//左边界 闭
@@ -71,7 +77,7 @@ module Cactus_s(					 //小仙人掌 small cactus
 				row_addr <= ROW_HIGHEST &&						//下边界 闭
 				row_addr > (ROW_HIGHEST-HEIGHT))				//上边界 开
 			
-				dout = data[addr];		    			//特定颜色
+				dout = data;		    							//特定颜色
 		end
 		else	//完成输出
 				dout =  12'hfff;    //白色
---
+++ Obstacle_layer.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:37:42 01/02/2020 
+// Design Name: 
+// Module Name:    Obstacle_layer 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Obstacle_layer(			 //障碍物图层
+	 input wire [31:0]clk_div,	 //整体时钟信号  参数可调
+										 //clk_div[1] 每隔多少时间从IP核中取出地址
+										 //clk_div[23] 每隔多少时间生成一个障碍物
+										 //clk_div[19] 障碍物每隔多少时间前进一列
+	 input wire [9:0] col_addr, // pixel ram row address, 480 (512) lines
+	 input wire [8:0] row_addr, // pixel ram col address, 640 (1024) pixels
+	 output wire [11:0] dout	    //障碍物的数据输出bbbb_gggg_rrrr     
+	 );
+
+	 parameter	
+		min  	   = 0,			 //最小图层编号
+		max      = 5;			 //最大图层编号
+
+	 reg  [max:0]start;		 //开始信号，对不同障碍物移动的开始信号
+	 wire [max:0]finish;	 	 //结束信号，障碍物移动结束
+	 wire [11:0]dout_c_s;
+	//产生随机数模块
+	integer seed;
+
+	reg [7:0] rand_num;	//[min,max]的值 此时是[0，5]
+	
+	initial begin 
+		seed = 0;
+		start <= 5'b0;
+	end
+
+
+	always @(posedge clk_div[23]) begin   //这个
+		rand_num <= min + {$random(seed)}%(max-min+1);	//生成随机信号
+		start <= 5'b0;
+		if(finish[rand_num]==0)//若该障碍物已经在屏幕当中，则生成下一种障碍物
+			rand_num <= min+(rand_num+1)%(max-min+1);
+		case(rand_num)
+			1:begin
+				start[0] <= 1;   //生成障碍物0  小仙人掌
+			end
+			default:;
+		endcase
+	end
+
+	
+   //小仙人掌模块
+   Cactus_s  layer1(.clk_ip(clk_div[1]),.clk(clk_div[19]),.start(start[0]),.col_addr(col_addr),.row_addr(row_addr),.dout(dout_c_s),.finish(finish[0]));
+	
+	
+	//dout
+		assign dout = dout_c_s;  //将不同的dout或起来
+
+
+
+endmodule
